53a419c8400892d6bafa2bbe7dca95816e02427f
Marcin Kunert <the.martines@gmail.com>
2022-05-05 12:57:52 +0200

    Rozwiazanie zad 4.2.

---
+++ src/main/java/pl/javastart/task/Person.java
@@ -0,0 +1,12 @@
+package pl.javastart.task;
+
+class Person {
+    String firstName;
+    String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
---
+++ src/main/java/pl/javastart/task/ToDoTask.java
@@ -0,0 +1,16 @@
+package pl.javastart.task;
+
+class ToDoTask {
+    boolean getHighPriority(Task task) {
+        return true;
+    }
+
+    boolean getMediumPriority(Task task) {
+        return true;
+    }
+
+    boolean getLowPriority(Task task) {
+        return true;
+    }
+
+}
